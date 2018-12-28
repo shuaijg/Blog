0905e62a2d9982873f81e9ecfb6f5cc861a7f778
shuaijg <15611771610@163.com>
2018-12-28 23:34:15 +0800

    前端访问后台node

--- server/app.js
+++ server/app.js
@@ -16,8 +16,9 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../dist/static/js')));
+//app.use(express.static(path.join(__dirname, '../dist/static/js')));
 //app.use(express.static(path.join(__dirname, '../src')));
+app.use(express.static(path.join(__dirname, '../dist')));
 
 app.use('/', indexRouter);
 
