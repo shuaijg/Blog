411479a4a8c75b445357428ddbf67aad5ed8e8b5
shuaijg <15611771610@163.com>
2018-12-30 18:04:17 +0800

    测试服务器上前端访问node

--- server/app.js
+++ server/app.js
@@ -11,12 +11,14 @@ var app = express();
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
+app.set('views', path.join(__dirname, '../dist/index.html'));
+app.set('view engine', 'html');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, '../dist/static/js')));
+app.use(express.static(path.join(__dirname, '../dist/static')));
 //app.use(express.static(path.join(__dirname, '../src')));
 
 app.use('/', indexRouter);
