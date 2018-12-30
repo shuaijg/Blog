5d42ebf070ceb43d4cdf576926cf37347db311c8
shuaijg <15611771610@163.com>
2018-12-30 19:08:35 +0800

    ceshi

--- server/app.js
+++ server/app.js
@@ -11,16 +11,13 @@ var app = express();
 // view engine setup
 //app.set('views', path.join(__dirname, 'views'));
 //app.set('view engine', 'jade');
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-//app.use(express.static(path.join(__dirname, '../dist/static/js')));
+app.use(express.static(path.join(__dirname, '../dist/static/js')));
 //app.use(express.static(path.join(__dirname, '../src')));
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 
